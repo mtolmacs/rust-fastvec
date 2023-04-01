cb1d9bf7b1f2c1b2969df117c3839377539e2437
Mark Tolmacs <mark@lazycat.hu>
2023-04-01 19:14:54 +0200

    Do something useful with the popped values in the benchmark.
    So that the compiler does not optimize away some code in the smallvec test.

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -9,37 +9,82 @@ use smallvec::SmallVec;
 struct Test(usize, &'static str);
 
 fn small_push_pop() {
-    #[benchmark_stage(i=10_000_000,name="fastvec")]
+    #[benchmark_stage(i=50_000_000,name="fastvec")]
     fn fastvec_small_push_pop() {
         let mut buf = FastVec::<Test, 3>::new();
         buf.push(Test(1, "A"));
         buf.push(Test(2, "B"));
         buf.push(Test(3, "C"));
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 3);
+        assert_eq!(val.1, "C");
+                
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 2);
+        assert_eq!(val.1, "B");
+        
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 1);
+        assert_eq!(val.1, "A");
     }
     
-    #[benchmark_stage(i=10_000_000,name="vec")]
+    #[benchmark_stage(i=50_000_000,name="vec")]
     fn vec_small_push_pop() {
         let mut buf = Vec::<Test>::new();
         buf.push(Test(1, "A"));
         buf.push(Test(2, "B"));
         buf.push(Test(3, "C"));
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 3);
+        assert_eq!(val.1, "C");
+                
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 2);
+        assert_eq!(val.1, "B");
+        
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 1);
+        assert_eq!(val.1, "A");
     }
     
-    #[benchmark_stage(i=10_000_000,name="smallvec")]
+    #[benchmark_stage(i=50_000_000,name="smallvec")]
     fn smallvec_small_push_pop<A: smallvec::Array<Item = Test>>() {
         let mut buf = smallvec::SmallVec::<A>::new();
         buf.push(Test(1, "A"));
         buf.push(Test(2, "B"));
         buf.push(Test(3, "C"));
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some());
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 3);
+        assert_eq!(val.1, "C");
+                
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 2);
+        assert_eq!(val.1, "B");
+        
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 1);
+        assert_eq!(val.1, "A");
     }
 
     fastvec_small_push_pop();
