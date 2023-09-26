dc709d096e4e3b0e59911e8b8a339aa3d10dc6ce
Mark Tolmacs <mark@lazycat.hu>
2023-09-26 10:44:39 +0200

    ArrayVec bench added for the small use case

---
+++ bench/NOTES.md
@@ -0,0 +1,6 @@
+# LLVM Optimization Remarks
+
+Emits a webpage containing the LLVM optimization remarks issued by optimization passes during compilation.
+
+## How to run
+`cargo install cargo-remarks` and the `cargo +nightly remark build`
--- bench/src/main.rs
+++ bench/src/main.rs
@@ -3,83 +3,84 @@ extern crate fastvec;
 extern crate bma_benchmark;
 extern crate smallvec;
 
+use arrayvec::ArrayVec;
 use fastvec::FastVec;
 use smallvec::SmallVec;
 
 struct Test(usize, &'static str);
 
 fn small_push_pop() {
-    #[benchmark_stage(i=50_000_000,name="fastvec")]
+    #[benchmark_stage(i = 10_000_000, name = "fastvec")]
     fn fastvec_small_push_pop() {
         let mut buf = FastVec::<Test, 3>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
 
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 3);
         assert_eq!(val.1, "C");
-                
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 2);
         assert_eq!(val.1, "B");
-        
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 1);
         assert_eq!(val.1, "A");
     }
-    
-    #[benchmark_stage(i=50_000_000,name="vec")]
+
+    #[benchmark_stage(i = 10_000_000, name = "vec")]
     fn vec_small_push_pop() {
         let mut buf = Vec::<Test>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
 
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 3);
         assert_eq!(val.1, "C");
-                
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 2);
         assert_eq!(val.1, "B");
-        
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 1);
         assert_eq!(val.1, "A");
     }
-    
-    #[benchmark_stage(i=50_000_000,name="smallvec")]
+
+    #[benchmark_stage(i = 10_000_000, name = "smallvec")]
     fn smallvec_small_push_pop<A: smallvec::Array<Item = Test>>() {
         let mut buf = smallvec::SmallVec::<A>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
 
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 3);
         assert_eq!(val.1, "C");
-                
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
         assert_eq!(val.0, 2);
         assert_eq!(val.1, "B");
-        
+
         let val = buf.pop();
         assert!(val.is_some());
         let val = val.unwrap();
@@ -87,28 +88,55 @@ fn small_push_pop() {
         assert_eq!(val.1, "A");
     }
 
+    #[benchmark_stage(i = 10_000_000, name = "arrayvec")]
+    fn arrayvec_small_push_pop<const Size: usize>() {
+        let mut buf = ArrayVec::<Test, Size>::new();
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 3);
+        assert_eq!(val.1, "C");
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 2);
+        assert_eq!(val.1, "B");
+
+        let val = buf.pop();
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.0, 1);
+        assert_eq!(val.1, "A");
+    }
+
+    arrayvec_small_push_pop::<8>();
     vec_small_push_pop();
     smallvec_small_push_pop::<[Test; 3]>();
     fastvec_small_push_pop();
 }
 
 fn large_push_pop() {
-    #[benchmark_stage(i=10_000_000,name="fastvec")]
+    #[benchmark_stage(i = 10_000_000, name = "fastvec")]
     fn fastvec_large_push_pop() {
         let mut buf = FastVec::<Test, 3>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
-        buf.push(Test(4, "D"));
-        buf.push(Test(5, "E"));
-        buf.push(Test(6, "F"));
-        buf.push(Test(7, "G"));
-        buf.push(Test(8, "H"));
-        buf.push(Test(9, "I"));
-        buf.push(Test(10, "J"));
-        buf.push(Test(11, "K"));
-        buf.push(Test(12, "L"));
-        buf.push(Test(13, "M"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
+        let _ = buf.push(Test(4, "D"));
+        let _ = buf.push(Test(5, "E"));
+        let _ = buf.push(Test(6, "F"));
+        let _ = buf.push(Test(7, "G"));
+        let _ = buf.push(Test(8, "H"));
+        let _ = buf.push(Test(9, "I"));
+        let _ = buf.push(Test(10, "J"));
+        let _ = buf.push(Test(11, "K"));
+        let _ = buf.push(Test(12, "L"));
+        let _ = buf.push(Test(13, "M"));
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
@@ -123,22 +151,22 @@ fn large_push_pop() {
         assert!(buf.pop().is_some());
     }
 
-    #[benchmark_stage(i=10_000_000,name="vec")]
+    #[benchmark_stage(i = 10_000_000, name = "vec")]
     fn vec_large_push_pop() {
         let mut buf = Vec::<Test>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
-        buf.push(Test(4, "D"));
-        buf.push(Test(5, "E"));
-        buf.push(Test(6, "F"));
-        buf.push(Test(7, "G"));
-        buf.push(Test(8, "H"));
-        buf.push(Test(9, "I"));
-        buf.push(Test(10, "J"));
-        buf.push(Test(11, "K"));
-        buf.push(Test(12, "L"));
-        buf.push(Test(13, "M"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
+        let _ = buf.push(Test(4, "D"));
+        let _ = buf.push(Test(5, "E"));
+        let _ = buf.push(Test(6, "F"));
+        let _ = buf.push(Test(7, "G"));
+        let _ = buf.push(Test(8, "H"));
+        let _ = buf.push(Test(9, "I"));
+        let _ = buf.push(Test(10, "J"));
+        let _ = buf.push(Test(11, "K"));
+        let _ = buf.push(Test(12, "L"));
+        let _ = buf.push(Test(13, "M"));
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
@@ -153,22 +181,22 @@ fn large_push_pop() {
         assert!(buf.pop().is_some());
     }
 
-    #[benchmark_stage(i=10_000_000,name="smallvec")]
+    #[benchmark_stage(i = 10_000_000, name = "smallvec")]
     fn smallvec_large_push_pop<A: smallvec::Array<Item = Test>>() {
         let mut buf = smallvec::SmallVec::<A>::new();
-        buf.push(Test(1, "A"));
-        buf.push(Test(2, "B"));
-        buf.push(Test(3, "C"));
-        buf.push(Test(4, "D"));
-        buf.push(Test(5, "E"));
-        buf.push(Test(6, "F"));
-        buf.push(Test(7, "G"));
-        buf.push(Test(8, "H"));
-        buf.push(Test(9, "I"));
-        buf.push(Test(10, "J"));
-        buf.push(Test(11, "K"));
-        buf.push(Test(12, "L"));
-        buf.push(Test(13, "M"));
+        let _ = buf.push(Test(1, "A"));
+        let _ = buf.push(Test(2, "B"));
+        let _ = buf.push(Test(3, "C"));
+        let _ = buf.push(Test(4, "D"));
+        let _ = buf.push(Test(5, "E"));
+        let _ = buf.push(Test(6, "F"));
+        let _ = buf.push(Test(7, "G"));
+        let _ = buf.push(Test(8, "H"));
+        let _ = buf.push(Test(9, "I"));
+        let _ = buf.push(Test(10, "J"));
+        let _ = buf.push(Test(11, "K"));
+        let _ = buf.push(Test(12, "L"));
+        let _ = buf.push(Test(13, "M"));
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
@@ -180,7 +208,7 @@ fn large_push_pop() {
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
         assert!(buf.pop().is_some());
-        assert!(buf.pop().is_some()); 
+        assert!(buf.pop().is_some());
     }
 
     smallvec_large_push_pop::<[Test; 3]>();
@@ -188,13 +216,12 @@ fn large_push_pop() {
     fastvec_large_push_pop();
 }
 
-
 fn main() {
     small_push_pop();
     staged_benchmark_print_for!("smallvec");
-    
+
     staged_benchmark_reset!();
-    
+
     large_push_pop();
     staged_benchmark_print_for!("smallvec");
 }
